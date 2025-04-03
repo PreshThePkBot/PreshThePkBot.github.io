db7054bab6a642b6cf2b82e0108971a8db6f7290
Presh <darklordrulez6@gmail.com>
2025-04-03 20:00:15 +0100

    why

---
+++ database.php
@@ -0,0 +1,32 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>PigeonCorp - Other</title> <!--The title believe it or not-->
+        <link rel="shortcut icon" href="images/logowhite.png" type="image.x-icon"> <!--links the favicon (website image in the title) from the images folder-->
+        <!--name final favicon 'favicon.ico'-->
+        <link rel="stylesheet" href="Style.css"> <!--links the css file-->
+
+        <!--Below are the fonts that will be used, then the head tag closes-->
+    </head>
+    <body>
+        <section class="other-header">
+            <nav>
+                <a href="index.html">Home</a>
+
+            </nav>
+
+                <div class="other-main">
+                    <h1>links to other programs/applications:</h1>
+                    <br>
+                    </br>
+                </div>
+
+                <div class="other-list">
+                    <br><p>Files coming soon</p></br>
+                </div>
+            </section>
+    
+        </section>
+
+    </body>
+</html>
